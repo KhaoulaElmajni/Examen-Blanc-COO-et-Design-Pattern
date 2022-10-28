7baffa2fbaf489520ed66dce56fa05aca1ca6ecf
Khaoula Elmajni <elmajnikhaoula99@gmail.com>
2022-10-28 12:09:14 +0100

    remove files

--- ExamenBlanc/src/main/java/me/elmajni/patterns/observer/Observable.java
+++
@@ -1,7 +0,0 @@
-package me.elmajni.patterns.observer;
-
-public interface Observable {
-    void addObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObservers();
-}
--- ExamenBlanc/src/main/java/me/elmajni/patterns/observer/Observer.java
+++
@@ -1,5 +0,0 @@
-package me.elmajni.patterns.observer;
-
-public interface Observer {
-    public void update(Parametrage parametrage);
-}
